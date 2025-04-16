b71248fdbda355eb7484c89bfcaccab96b994099
Ar2095 <aanyarawal@hotmail.com>
2025-04-16 01:45:23 +0000

    dropdown

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-import streamlit as st
+import os
 import pandas as pd
 import numpy as np
-
+from collections import defaultdict
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import streamlit as st
 from disease_classifier import predict_user_disease
 
 
 if __name__ == "__main__":
     st.title('Symptom-Disease Matcher')
     st.subheader("Enter your symptoms to learn what disease/issue your body may be experiencing. Type in each symptom separared by commas.")
-    user_input  = st.chat_input("Type your symptoms here")
-
-    if user_input:
-        st.write(f"You entered: {user_input}")
-        symptoms = user_input.split(",");  
-        cleaned_symptoms = []; 
-        for symptom in symptoms:
-            symptom = symptom.strip();
-            cleaned_symptoms.append(symptom)
-        print(cleaned_symptoms);
-        st.write(predict_user_disease(cleaned_symptoms)); 
+    #user_input  = st.chat_input("Type your symptoms here")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    training_data_path = os.path.join(BASE_DIR, "training_dataset.csv")
+    df = pd.read_csv(training_data_path)
+    X = df.columns[1:].tolist()
+    y = df['disease']
+    symptoms = st.multiselect('Select Your Symptoms: ', X)
+    if (st.button("Predict")):
+        st.write(predict_user_disease(symptoms)); 
         
